f2208560828653913069bb661763fe7adbbe7206
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-09-22 12:47:38 -0700

    Changed Ships function to create different instances of ships

--- js/main.js
+++ js/main.js
@@ -3,22 +3,15 @@ var startGame = function(){
   return false;
 };
 
-function Ship(shipName, shipSize){
-  this.shipName = shipName || "";
+function Ship(shipSize,shipType){
   this.shipSize = shipSize || 0;
-  // return false;
+  this.shipType = shipType;
 }
-
-function Submarine(shipName, shipSize, shipType) {
-  this.base = Ship;
-  this.base(shipName,shipSize);
-  this.shipType = "Submarine";
-}
-
-Submarine.prototype = new Ship();
-
-var smallSub = new Submarine("Submarine", 3);
-
+var submarine = new Ship(3, "Submarine");
+var destroyer = new Ship(3,"Destroyer");
+var patrolBoat = new Ship(2, "Patrol Boat");
+var battleship = new Ship(4, "Battleship");
+var aircraftCarrier = new Ship(5, "Aircraft Carrier");
 
 
 var Players = function(){
