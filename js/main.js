faed647e5a27506e9791b6a798bc5a6270dc0efb
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-09-22 10:54:26 -0700

    Update to main.js: add buildGameBoard function.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
-
-
-var startGame = function(){	
-	return false;
-
-}
-
-
+var startGame = function(){
+  return false;
+};
 
 var Ships = function(){
-
-	return false;
-}
-
+  return false;
+};
 
 var Players = function(){
-	return false;
-}
+  return false;
+};
 
 
 var Scoreboard = function(){
-	return false;
-}
+  return false;
+};
 
+
+
+
+var buildGameBoard = function(boardSize) {
+  var gameBoard = [];
+
+  for (var row = 0; row < boardSize; row++) {
+    var boardRow = [];
+    for (var column = 0; column < boardSize; column++) {
+      // We are creating an Object (can create constructor in another location)
+      var squareTile = {
+        position: [row, column]
+      };
+      boardRow.push(squareTile);
+    }
+    gameBoard.push(boardRow);
+  }
+  return gameBoard;
+};
+
+var battleShipBoard = buildGameBoard(8);
