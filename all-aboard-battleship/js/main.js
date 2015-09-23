f4c0397ee8fd21ab46a06fd8c78c2ebe1176c80b
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-09-22 19:56:15 -0700

    Add click handler (hard-code) for the first tile on board

--- all-aboard-battleship/js/main.js
+++ all-aboard-battleship/js/main.js
@@ -1,24 +1,44 @@
 
+var startGame = function(){
+  return false;
+};
 
-var startGame = function(){	
-	return false;
-
+function Ship(shipSize,shipType){
+  this.shipSize = shipSize || 0;
+  this.shipType = shipType;
 }
 
-
-
-var Ships = function(){
-
-	return false;
-}
+var submarine = new Ship(3, "Submarine");
+var destroyer = new Ship(3,"Destroyer");
+var patrolBoat = new Ship(2, "Patrol Boat");
+var battleship = new Ship(4, "Battleship");
+var aircraftCarrier = new Ship(5, "Aircraft Carrier");
 
 
 var Players = function(){
-	return false;
-}
+  return false;
+};
 
 
 var Scoreboard = function(){
-	return false;
+  return false;
+};
+
+
+var shipSink = function(){
+  return false;
 }
 
+var shipHits = function(){
+  return false;
+}
+
+var markTile = function() {
+  $("#position-00").html("X");
+};
+
+$("#position-00").click(markTile);
+
+// function SquareTile() {
+//   position:
+// }
