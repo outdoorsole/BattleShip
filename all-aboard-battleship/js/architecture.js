efc0c4548f25803b53c0edb919d6d559f6d1aaa6
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-09-22 17:35:52 -0700

    Start of placing ships on the board.

--- all-aboard-battleship/js/architecture.js
+++ all-aboard-battleship/js/architecture.js
@@ -1,11 +1,41 @@
 // Square piece
 //   - holds the position [row, column]
-//   - holds ship info (which part of the ship)
+//   - holds ship info (ship index of array)
+//   - marked tile info
+
 
 // Ship
 //   - length of ship
 //   - name of ship (shipType)
-//   - ship marked [0, 0, 0] by default
+//   - ex: shipHit [0, 0, 0] by default
+//   - will change mark to 1
+//   - ship is considered sunk when the array is filled with 1's
+//   -
+
+
+//Ship properties:
+// shipLength;
+// shipType;
+// shipHit;
+
+
+// Square object (within gameboard)
+// boardPosition;
+// shipObject (? - pass in length of ship, use index to access shipHit)
+//
+
+// Init gameboard
+
+
+
+// Placing ships on the board
+
+
+
+// Ship is placed
+
+// Calculate Points
+//   -
 
 // The default is [0]
 // This means that a tile is not marked
@@ -13,13 +43,19 @@
 
 var shipSize = 3;
 
-var checkIfTileMarked = function() {
-  var isMarked = [];
+var createShipMarkedArray = function() {
+  var isNotHit = [];
   for (var i = 0; i < shipSize; i++) {
-    isMarked.push(0);
+    isNotMarked.push(0);
   }
-  return isMarked;
+  return isNotHit;
 }
 
 // var shipSize = 3;
-// assert(checkIfTileMarked().length === [0, 0, 0], "isMarked returns an array of [0, 0, 0] when the shipSize is 3")
+// assert(createShipMarkedArray().length === [0, 0, 0], "isMarked returns an array of [0, 0, 0] when the shipSize is 3");
+
+var checkIfMarked = function(array) {
+
+}
+
+
