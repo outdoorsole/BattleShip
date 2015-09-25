3a6bcd3b1f27acbe092037019316b2c8da594459
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-09-25 14:58:24 -0700

    Worked on composition. Created individual files for corresponding assert tests.

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,13 +1,14 @@
-function allowDrop(ev) {
-  ev.preventDefault();
-}
+//May utilize later - not applicable at this point.
+// function allowDrop(ev) {
+//   ev.preventDefault();
+// }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
+// function drag(ev) {
+//   ev.dataTransfer.setData("text", ev.target.id);
+// }
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
+// function drop(ev) {
+//   ev.preventDefault();
+//   var data = ev.dataTransfer.getData("text");
+//   ev.target.appendChild(document.getElementById(data));
+// }
---
+++ js/gamemanager.js
@@ -0,0 +1,64 @@
+var GameManager = function() {
+  this.startGame = function() {
+   this.listShips();
+   this.showGameBoard();
+  };
+
+//will be expanding ship location information to gameboard //
+  var gameboard = new GameBoard("position-00");
+  this.showGameBoard = function(){
+    return gameboard;
+  }
+
+  var submarine = new Ship(3, "Submarine");
+  var destroyer = new Ship(3, "Destroyer");
+  var patrolBoat = new Ship(2, "Patrol Boat");
+  var battleship = new Ship(4, "Battleship");
+  var aircraftCarrier = new Ship(5, "Aircraft Carrier");
+
+  var shipArray = [submarine, destroyer, patrolBoat, battleship, aircraftCarrier];
+
+  this.listShips = function() {
+    return shipArray;
+  }
+
+  var selectedShip = 0;
+
+  this.setSelectedShip = function(length){
+    selectedShip = length;
+  }
+
+  this.setLocation = function(num) {
+
+    //adds an additional number to match the length of the position id so that the first row becomes a double digit string//
+    if (num >= 0 && num <= 9) {
+      var nextPosition = num;
+      for (var i = 0; i < selectedShip; i++) {
+        $("#position-0" + nextPosition).css("background-color", "red");
+        nextPosition++;
+      }
+    }
+    //else, use original number for the position id.
+    else {
+      var nextPosition = num;
+      for (var i = 0; i < selectedShip; i++) {
+        $("#position-" + nextPosition).css("background-color", "red");
+        nextPosition++;
+      }
+    }
+    //marks the ship position in the array
+    for(var i = 0; i < selectedShip; i++) {
+    //note that this is currently hardcoded to submarine - will be refactored to match selectedShip//
+      submarine.shipLocation.push(num + i);
+    }
+  };
+
+  var markTile = function(e) {
+    console.log(e);
+    $("#" + e.target.id).html("X");
+  }
+
+  var getPositions = function() {
+    $(".tile").click(markTile);
+  }
+}
