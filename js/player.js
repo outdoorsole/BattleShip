d7daa33ad42110647f6c90c2634b9802952f1e01
ddsooxo <dchuteng@gmail.com>
2015-09-27 10:32:09 -0700

    commiting files from player-tests

--- js/player.js
+++ js/player.js
@@ -1,6 +1,23 @@
-var Players = function(){
-  return false;
+var Player = function(name,shipHit,shipMiss){
+	this.name = name || "";
+	this.shipHit = [];
+	this.shipMiss = [];
+	return this.shipHit;
 };
+
+var player1 = new Player("Player1");
+var player2 = new Player("Player2");
+
+
+
+//properties: name, 2 empty array: shipHit, shipMiss
+
+
+
+
+
+
+
 
 //player is responsible for selecting tiles on the board they wish to
 //launch missiles at
--- test-app/js/ship-tests.js
+++ test-app/js/ship-tests.js
@@ -1,12 +1,12 @@
-// Make some test cases first
-// Set up work
-// Make a few different ships
-// Set them to have certain properties
-// 1) ship sunk ; 2) ship not sunk
+// // Make some test cases first
+// // Set up work
+// // Make a few different ships
+// // Set them to have certain properties
+// // 1) ship sunk ; 2) ship not sunk
 
-var shouldBeSunkShip = new Ship(2, "Sunk Ship");
-var result = shouldBeSunkShip.shipHit();
-assert( result === false, "The ship should not be sunk in this case." );
+// var shouldBeSunkShip = new Ship(2, "Sunk Ship");
+// var result = shouldBeSunkShip.shipHit();
+// assert( result === false, "The ship should not be sunk in this case." );
 
-var result2 = shouldBeSunkShip.shipHit();
-assert( result2 === true, "The ship should be sunk in this case." );
+// var result2 = shouldBeSunkShip.shipHit();
+// assert( result2 === true, "The ship should be sunk in this case." );
