2fa755df19aa2860c767f47d69e1abcb8182fb6f
A-C-Existence <A.C.Existence@gmail.com>
2015-09-27 10:34:04 -0700

    Worked on assert tests for game manager.

--- test-app/js/gamemanager-tests.js
+++ test-app/js/gamemanager-tests.js
@@ -9,8 +9,50 @@ var gameManager = new GameManager();
 // construct ships
 
 
-var testGameBoard = new GameBoard();
-// gameBoard === ??some-property-of-gameboard.....
+assert(submarine === ship), "submarine should be an instance of ship if game has initiated and the ship constructor is available");
+assert(player2.missArr === [], "play two's array should be empty");
+assert("should invoke btn")
+assert(" ('click' should invoke game setup state")
+var objInstance = Object.create(PublicModule);  
+objInstance.init({  
+  valueSetOnInit: "value set during initialization"
+});
+
+
+//err just trying something out - I know this is not
+//the proper way to go about testing this/ it may not be necessary
+//or considered too complicated but I was curious to see if it was possible
+
+
+describe("Button Click Event Tests", function() {
+  var spyEvent;
+   
+  beforeEach(function() {
+    setUpHTMLFixture();
+  });
+      
+  assert("should invoke the btnShowMessage click event.", function() {
+    spyEvent = spyOnEvent('#btnShowMessage', 'click');
+    $('#btnShowMessage').trigger( "click" );
+       
+    expect('click').toHaveBeenTriggeredOn('#btnShowMessage');
+    expect(spyEvent).toHaveBeenTriggered();
+  });
+      
+assert("should invoke the btnHideMessage click event.", function() {
+    spyEvent = spyOnEvent('#btnHideMessage', 'click');
+    $('#btnHideMessage').trigger( "click" );
+       
+    expect('click').toHaveBeenTriggeredOn('#btnHideMessage');
+    expect(spyEvent).toHaveBeenTriggered();
+  });
+});
+
+//by accessing player one and two we know that game manager
+//has the ability to switch turns ?
+
+
+
 
 assert(, "returns a new instance of GameManager");
 assert(player1.missArr === [], "player one's array should be empty");
