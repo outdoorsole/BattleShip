5ee985c51eb05ac835c248dd01a4ca7013a1f94f
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-09-27 10:32:19 -0700

    Staged to merge to master branch.

--- test-app/js/ship-tests.js
+++ test-app/js/ship-tests.js
@@ -15,4 +15,6 @@ PlayerNumber: the player the ship belongs to
 
 var submarine = new Ship("Submarine", 3);
 
+// assert(submarine instanceof Ship, "A ship was created from the ship constructor");
+
 
